56098bd53b514f6441b5a3f543c94eaf8c39b84e
giangcm <rockieman777@gmail.com>
2023-02-21 15:06:58 +0100

    create main and nav

--- ressursarkiv/src/components/Header.js
+++ ressursarkiv/src/components/Header.js
@@ -1,4 +1,4 @@
 export default function Header() {
-    return(
-        <h1>Ressursarkiv</h1>)
+    return <h1>Ressursarkiv</h1>
+        
 }
---
+++ ressursarkiv/src/components/Main.js
@@ -0,0 +1,19 @@
+export default function Main(){
+    return (
+        <article id= "content">
+        <h3>HTML</h3>
+        <p>
+          HTML står for HyperText Markup Language, og er et strukturspråk som
+          brukes for å lage strukturer til nettside- og applikasjonsgrensesnitt.
+        </p>
+        <div id="link">
+          <a target="_blank" href="https://www.w3schools.com/html/">
+            W3Schools</a>
+          <a target="_blank" href="https://html.spec.whatwg.org/multipage/">
+            HTML Living standard</a>
+          <a target="_blank" href="https://html.com/"> HTML.com Tutorials</a>
+        </div>
+
+        </article>
+    )
+}
