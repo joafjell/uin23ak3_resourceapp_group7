3fc2978f4cab3a64a3e3074c264353073013d546
giangcm <rockieman777@gmail.com>
2023-02-23 14:21:24 +0100

    Update Main.js

--- ressursarkiv/src/components/Main.js
+++ ressursarkiv/src/components/Main.js
@@ -2,10 +2,6 @@ export default function Main(){
     return (
         <article id= "content">
         <h3>HTML</h3>
-        <p>
-          HTML står for HyperText Markup Language, og er et strukturspråk som
-          brukes for å lage strukturer til nettside- og applikasjonsgrensesnitt.
-        </p>
         <div id="link">
           <a target="_blank" href="https://www.w3schools.com/html/">
             W3Schools</a>
