6bd94ffc3ac78b7c933e45edbf51e094ebdc8b02
vetleenb <122876020+vetleenb@users.noreply.github.com>
2023-02-27 10:41:14 +0100

    Update Nav.js

--- ressursarkiv/src/components/Nav.js
+++ ressursarkiv/src/components/Nav.js
@@ -1,9 +1,13 @@
 export default function Nav(){
     return (
         <button onclick="innhold(0)" class="btnone">Kategori 1</button>
+        
         <button onclick="innhold(1)" class="btntwo">Kategori 2</button>
+        
         <button onclick="innhold(2)" class="btnthree">Kategori 3</button>
+        
         <button onclick="innhold(3)" class="btnfour">Kategori 4</button> 
+        
         <button onclick="innhold(4)" class="btnfive">Kategori 5</button>
       
         
