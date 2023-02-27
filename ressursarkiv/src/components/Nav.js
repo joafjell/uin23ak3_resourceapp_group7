934b12e9a4d77ef1e8e6643a01a74d8c380b04d7
vetleenb <122876020+vetleenb@users.noreply.github.com>
2023-02-27 11:29:08 +0100

    Update Nav.js

--- ressursarkiv/src/components/Nav.js
+++ ressursarkiv/src/components/Nav.js
@@ -1,12 +1,12 @@
 export default function Nav(){
     return (
-        
+        <div>
         <link></link> 
         <link></link>
         <link></link>
         <link></link>
         <link></link>
-        
+        </div>
         
         
         
