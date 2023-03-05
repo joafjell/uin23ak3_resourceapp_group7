d3fb4dfdbea13e8d14457aace107cfa1e94cccac
vetleenb <122876020+vetleenb@users.noreply.github.com>
2023-03-05 17:25:08 +0100

    Update Nav.js

--- ressursarkiv/src/components/Nav.js
+++ ressursarkiv/src/components/Nav.js
@@ -1,14 +1,18 @@
 export default function Nav(){
     return (
         <div>
-        <link></link> 
-        <link></link>
-        <link></link>
-        <link></link>
-        <link></link>
+        <ul className="navBtns">
+        <li><link to="">Kategori_1</link></li>
+        <li><link to="">Kategori_2</link></li>
+        <li><link to="">Kategori_3</link></li>
+        <li><link to="">Kategori_4</link></li>
+        <li><link to="">Kategori_5</link></li>
+        </ul>
         </div>
         
         
         
         )
 }
+
+export default Nav;
