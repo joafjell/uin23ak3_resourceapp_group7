1d04316771c9343dac8a047260072d5a13939e7a
vetleenb <122876020+vetleenb@users.noreply.github.com>
2023-03-07 13:36:22 +0100

    Update App.js

--- ressursarkiv/src/App.js
+++ ressursarkiv/src/App.js
@@ -2,6 +2,16 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import './css/main.css';
+/*import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import { HTML } from "./";
+import { CSS } from "./";
+import { Javascript } from "./";
+import { React } from "./";
+import { Sanity } from "./";
+import { Navbarr } from "./components/Nav";*/
+
+
+
 
 function App() {
     return(
