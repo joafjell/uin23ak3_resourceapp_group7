ac730ddf17d80c8a9586f6312cdcf46f76aa555c
muontian <89585394+muontian@users.noreply.github.com>
2023-03-09 08:37:43 +0100

    noen forandringer

--- ressursarkiv/src/components/Information.js
+++ ressursarkiv/src/components/Information.js
@@ -3,7 +3,6 @@
 export default function Information({title, url}){
     return(
         <article className="info-card">
-            <h3>{title}</h3>
             <ul>
                 <li>
                 <a class="links" href={url}>{title}</a>
