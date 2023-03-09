e5ea72353b6fddb270373a1d03d317f08980a108
giangcm <rockieman777@gmail.com>
2023-03-09 09:12:54 +0100

    new

--- ressursarkiv/src/components/Layout.js
+++ ressursarkiv/src/components/Layout.js
@@ -1,16 +1,13 @@
-import { Link, Outlet } from "react-router-dom";
+
 import Header from "./Header";
 
 export default function Layout(){
     return(
         <div id="container">
             <header>  
-                <Link> 
-                    <Header />
-                </Link>
+                
             </header>
             <main>
-                <Outlet/>
             </main>
             <footer>
 
