81139293a0ebcf5256027bfc451a051d6ebb8f61
giangcm <rockieman777@gmail.com>
2023-03-09 09:48:11 +0100

    Update App.js

--- ressursarkiv/src/App.js
+++ ressursarkiv/src/App.js
@@ -10,7 +10,7 @@ import Layout from './components/Layout';
 
 function App() {
     <Layout>
-        <Routing />
+        
     </Layout>
     const [currentTitleState, setCurrentTitleState] = useState("html");
     return (
